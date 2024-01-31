ccb1b2f03eed0159fb0f5929733cbc2dd49a0b4a
Justin Dhillon <justin.singh.dhillon@gmail.com>
2024-01-30 17:56:08 -0800

    167

---
+++ 167. Two Sum II - Input Array Is Sorted/solution.ts
@@ -0,0 +1,16 @@
+function twoSum(numbers: number[], target: number): number[] {
+    let left: number = 0;
+    let right: number = numbers.length-1;
+
+    while (true) {
+        const sum: number = numbers[left] + numbers[right];
+        
+        if (sum < target) {
+            left++;
+        } else if (sum > target) {
+            right--;
+        } else {
+            return [left+1, right+1];
+        }
+    }
+};
