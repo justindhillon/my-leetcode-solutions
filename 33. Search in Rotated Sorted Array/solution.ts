dfd086a6df87f6fff13c77b66bbfeca829929d76
Justin Dhillon <justin.singh.dhillon@gmail.com>
2024-02-10 17:22:54 -0800

    33

---
+++ 33. Search in Rotated Sorted Array/solution.ts
@@ -0,0 +1,27 @@
+function search(nums: number[], target: number): number {
+    let left = 0;
+    let right = nums.length-1;
+
+    while (left <= right) {
+        const middle = Math.floor((left+right)/2);
+        
+        if (nums[middle] === target)
+            return middle
+
+        if (nums[left] <= nums[middle]) {
+            if (nums[left] <= target && target < nums[middle]) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        } else {
+            if (nums[middle] < target && target <= nums[right]) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+    }
+
+    return -1; 
+};
