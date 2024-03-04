efe0d4e170fea028e38669118dd719cd4495c3cc
Justin Dhillon <justin.singh.dhillon@gmail.com>
2024-03-04 11:57:56 -0800

    230

---
+++ 230. Kth Smallest Element in a BST/solution.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function kthSmallest(root: TreeNode | null, k: number): number {
+    var stack = [];
+    while (true) {
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+
+        root = stack.pop();
+        k--;
+
+        if (k == 0) 
+            return root.val;
+
+        root = root.right;
+    }
+};
